25fdc9ea41f8ddeccf0653eaadc1b143e7a3b39e
StabbyCutyou <sean.kelly@tapjoy.com>
2014-09-29 10:50:41 -0400

    Fixing reference to airbrake

--- lib/chore/airbrake.rb
+++ lib/chore/airbrake.rb
@@ -12,16 +12,16 @@ module Chore
       # Require this gem and call this function to enable airbraking all exceptions thrown from jobs
       # use Chore.config.airbrake to set airbrake options for sending notifications
 
-      unless Airbrake.configuration.api_key
+      unless ::Airbrake.configuration.api_key
         raise "Chore airbrake support requires that Airbrake is already configured but the API key is not currently set"
       end
       
-      Airbrake.configuration.async do |notice|
-        Thread.new { Airbrake.sender.send_to_airbrake(notice) }
+      ::Airbrake.configuration.async do |notice|
+        Thread.new { ::Airbrake.sender.send_to_airbrake(notice) }
       end
       
       Chore.add_hook(:on_failure) do |msg,error|
-        Airbrake.notify(error, Chore::Airbrake.build_reporting_options_for(msg))
+        ::Airbrake.notify(error, Chore::Airbrake.build_reporting_options_for(msg))
       end
       
       Chore.add_hook(:within_fork) do |worker, &block|
@@ -29,7 +29,7 @@ module Chore
           block.call(worker)
         rescue StandardError => e
           message = { :body => 'Error within fork.', :messages => worker.work.map(&:message) }
-          Airbrake.notify(e, Chore::Airbrake.build_reporting_options_for(message))
+          ::Airbrake.notify(e, Chore::Airbrake.build_reporting_options_for(message))
           raise e
         end
       end
